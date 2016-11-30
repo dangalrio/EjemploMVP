97d54951d78e6a8fad1fa50e016f3fa2e19cff4e
Daniel Gallardo Ríos <Daniel Gallardo Ros>
2016-11-30 17:03:19 +0100

    Prueba commit

---
+++ app/src/main/java/pruebas/dangalrio/com/pruevasvn/MainActivity.java
@@ -0,0 +1,13 @@
+package pruebas.dangalrio.com.pruevasvn;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
