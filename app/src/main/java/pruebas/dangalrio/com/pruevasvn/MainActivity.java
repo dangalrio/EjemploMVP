3786f87cdefed7d64560307ae36c9643061730a7
dangalrio <danigallardorios@gmail.com>
2016-12-04 00:26:37 +0100

    Primer botón con la muestra de una actividad con un botón flotante

--- app/src/main/java/pruebas/dangalrio/com/pruevasvn/MainActivity.java
+++ app/src/main/java/pruebas/dangalrio/com/pruevasvn/MainActivity.java
@@ -1,13 +1,27 @@
 package pruebas.dangalrio.com.pruevasvn;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button boton_flotante_extendido;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        boton_flotante_extendido = (Button) findViewById(R.id.btn_float_extended);
+
+        boton_flotante_extendido.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(view.getContext(),FloatButtonExtendedActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
